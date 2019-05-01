fde2f55adece81b381634da1075b40674d5aa3de
hiphop2531 <30566327+hiphop2531@users.noreply.github.com>
2019-05-01 20:38:12 +0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -6,6 +6,6 @@
 rm -f install
 kguza=$1
 cd
-wget -q -O lock.sh http://smile-vpn.net/K/scrip/u-d/install.sh/lock.sh
+wget -q -O lock.sh https://github.com/hiphop2531/Pritunl-All-OS-Kruza/blob/master/lock.sh
 chmod +x lock.sh
 ./lock.sh $kguza
