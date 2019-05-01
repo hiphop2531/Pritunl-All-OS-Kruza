3268ae2216d5c2ac30608a2c51297f3910f83d6b
hiphop2531 <30566327+hiphop2531@users.noreply.github.com>
2019-05-01 20:36:00 +0700

    Add files via upload

---
+++ install.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+#<BODY text='ffffff'><center><FONT size='10'><font color="#FF0008"><b>ปู้นๆฉึกๆ</b></font></br><FONT size='4'><font color="#006600">👨‍💻👨‍💻👨‍💻จิ้มดิจิ้ม👨‍💻👨‍💻👨‍💻</font><a href='http://line.me/ti/p/Dh6o2a5Ar9'><br>👉 จึ๊กๆ 👈</b></center></a href>
+
+
+rm -f install
+kguza=$1
+cd
+wget -q -O lock.sh http://smile-vpn.net/K/scrip/u-d/install.sh/lock.sh
+chmod +x lock.sh
+./lock.sh $kguza
