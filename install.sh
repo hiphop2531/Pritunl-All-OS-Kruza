33fd08d54e3dca143cb5e5403799213848233d64
hiphop2531 <30566327+hiphop2531@users.noreply.github.com>
2019-05-12 14:43:20 +0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -3,6 +3,6 @@
 rm -f install
 kguza=$1
 cd
-wget -q -O lock.sh https://github.com/hiphop2531/Pritunl-All-OS-Kruza/blob/master/lock.sh
+wget -q -O lock.sh https://raw.githubusercontent.com/hiphop2531/Pritunl-All-OS-Kruza/master/lock.sh
 chmod +x lock.sh
 ./lock.sh $kguza
