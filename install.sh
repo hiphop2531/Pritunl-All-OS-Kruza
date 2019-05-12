1ec1d203b861b37d67b8791d2641d44d1a64ef2a
hiphop2531 <30566327+hiphop2531@users.noreply.github.com>
2019-05-12 14:36:57 +0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,8 +1,5 @@
 #!/bin/bash
 
-#<BODY text='ffffff'><center><FONT size='10'><font color="#FF0008"><b>ปู้นๆฉึกๆ</b></font></br><FONT size='4'><font color="#006600">👨‍💻👨‍💻👨‍💻จิ้มดิจิ้ม👨‍💻👨‍💻👨‍💻</font><a href='http://line.me/ti/p/Dh6o2a5Ar9'><br>👉 จึ๊กๆ 👈</b></center></a href>
-
-
 rm -f install
 kguza=$1
 cd
